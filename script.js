7b2323c56865086381a236c48740939b9d859c89
feriel78 <ferielbenhess@gmail.com>
2026-03-03 23:08:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Vérifier que JS fonctionne
+console.log("JavaScript connecté !");
+
+// Animation bouton
+const button = document.getElementById("ctaBtn");
+
+button.addEventListener("click", function() {
+  alert("Bienvenue sur EQUIVIA !");
+});
